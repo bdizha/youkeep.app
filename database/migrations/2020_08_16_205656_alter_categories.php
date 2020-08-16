ffa5e05fb69d5db2825be18b4a0876ad4a1e5889
Batanayi Matuku <bdizha@gmail.com>
2020-08-16 23:06:37 +0200

    Removed old migrations

--- database/migrations/2020_08_16_205656_alter_categories.php
+++ database/migrations/2020_08_16_205656_alter_categories.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class AlterCategories extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::table('categories', function (Blueprint $table) {
+            $table->smallInteger('type')->index()->default(2);
+            $table->dropColumn('is_featured');
+            $table->dropColumn('is_main');
         });
     }
 
@@ -27,6 +27,8 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::table('categories', function (Blueprint $table) {
+            //
+        });
     }
 }
