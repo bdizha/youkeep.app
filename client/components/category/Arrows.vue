7bbaeee4b7bbacdbe384ad098bcc8306407a406f
Batanayi Matuku <bdizha@gmail.com>
2020-11-21 20:23:42 +0200

    Removed has data checks

--- client/components/category/Arrows.vue
+++ client/components/category/Arrows.vue
@@ -1,7 +1,7 @@
 <template>
   <a-row class="r-slider" type="flex" justify="center" align="middle">
     <a-col class="r-ph-12" :xs="{ span: 24 }" :sm="{ span: 24 }" :lg="{ span: 24 }">
-      <VueSlickCarousel v-if="hasData && hasCategories" v-bind="settings">
+      <VueSlickCarousel v-if="hasCategories" v-bind="settings">
         <nuxt-link class="r-slider-item r-text-view-more"
                    :class="'r-slider-item-' + size"
                    v-for="(category, index) in categories"
