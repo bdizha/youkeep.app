d0b7ddd4423e905efa41ffb5bed4c206b269cc89
Batanayi Matuku <bdizha@gmail.com>
2021-01-03 22:00:51 +0200

    Modified the product photos updater

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -14,51 +14,22 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
+
+        echo ">>>> Start product photos update";
+
         $this->_updateProductPhotos();
 
-        die("Well done.");
-    }
-
-    private function _deleteIncorrectProducts()
-    {
-        $products = Product::orderBy('created_at', 'DESC')->get();
-
-        $totalPurges = 0;
-        foreach ($products as $product) {
-            $photo = public_path('storage/product/' . $product->photo);
-            $thumbnail = public_path('storage/product/' . $product->thumbnail);
-            if (!file_exists($photo) || !file_exists($thumbnail)) {
-                echo "Not found images for product {$products->slug}:\n";
-                echo $photo . "\n";
-
-                $totalPurges++;
-
-                \App\CategoryProduct::where('product_id', $product->id)->delete();
-                \App\ProductPhoto::where('product_id', $product->id)->delete();
-                \App\ProductVariant::where('product_id', $product->id)->delete();
-                \App\StoreProduct::where('product_id', $product->id)->delete();
-
-                $product->categories()->delete();
-                $product->delete();
-            } else {
-                echo "Found images for product {$products->slug}:\n";
-                echo $photo . "\n";
-            }
-        }
-
-        echo "<> Total deleted {$totalPurges} products <>\n";
+        die("End product photos update <<<<");
     }
 
     private function _updateProductPhotos()
     {
         $products = Product::orderBy('created_at', 'DESC')
-            ->whereNotNull('store_id')
             ->get();
 
-        dd($products[0]->store);
-
-
         $totalPurges = 0;
+        $totalPhotosPurges = 0;
+
         foreach ($products as $product) {
             $photos = ProductPhoto::where('product_id', $product->id)
                 ->take(1)
@@ -75,14 +46,24 @@ class ProductSeeder extends Seeder
                 $totalPurges++;
             } else {
                 foreach ($photos as $photo) {
-                    $product->thumbnail = $photo->thumb;
-                    $product->photo = $photo->image;
-                    $product->save();
-                    break;
+                    $image = public_path('storage/product/' . $photo->image);
+                    $thumbnail = public_path('storage/product/' . $photo->thumb);
+
+                    if (!file_exists($image) || !file_exists($thumbnail)) {
+                        echo "Deleted photo {$photo->image}" . "\n";
+
+                        $totalPhotosPurges++;
+                        \App\ProductPhoto::where('id', $photo->id)->delete();
+                    }else{
+                        $product->thumbnail = $photo->thumb;
+                        $product->photo = $photo->image;
+                        $product->save();
+                    }
                 }
             }
         }
 
-        echo "<> Total deleted {$totalPurges} products <>\n";
+        echo "<> Total deleted {$totalPurges} products\n";
+        echo "<> Total deleted {$totalPhotosPurges} product photos <>\n";
     }
 }
