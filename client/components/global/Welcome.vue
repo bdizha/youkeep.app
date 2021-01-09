b20662acbf0b490f661a25d22d947971d903787c
Batanayi Matuku <bdizha@gmail.com>
2021-01-09 22:26:18 +0200

    Enhanced category list margins

--- client/components/global/Welcome.vue
+++ client/components/global/Welcome.vue
@@ -11,7 +11,9 @@
           <r-category-slider v-if="processes.isFixed"></r-category-slider>
         </a-col>
       </a-row>
-      <r-category-list :columns="6" :limit="6"></r-category-list>
+      <div class="r-margin-out-sm" style="margin: 0 -24px;">
+        <r-category-list :columns="6" :limit="6"></r-category-list>
+      </div>
     </a-col>
   </a-row>
 </template>
