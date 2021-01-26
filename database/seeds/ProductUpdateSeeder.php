8132b522665ab88e2271a56c822288e3661a20db
Batanayi Matuku <bdizha@gmail.com>
2021-01-26 03:18:52 +0200

    Fixed issue with clearling product photos

--- database/seeds/ProductUpdateSeeder.php
+++ database/seeds/ProductUpdateSeeder.php
@@ -26,25 +26,27 @@ class ProductUpdateSeeder extends Seeder
                 ->get();
 
             if (!empty($photos[0])) {
-                if (!file_exists(public_path('storage/product/' . $photos[0]['image'])) ||
-                    !file_exists(public_path('storage/product/' . $photos[0]['thumb']))) {
-                    \App\ProductPhoto::where('id', $photos[0]['id'])
-                        ->delete();
+                foreach ($photos as $photo) {
+                    if (!file_exists(public_path('storage/product/' . $photo['image'])) ||
+                        !file_exists(public_path('storage/product/' . $photo['thumb']))) {
+                        \App\ProductPhoto::where('id', $photo['id'])
+                            ->delete();
 
-                    echo "Deleted Product Photo >>>> " . public_path('storage/product/' . $photos[0]['thumb']) . " \n";
-                } else {
-                    echo "Skipped Product Photo >>>> {$photos[0]['image']} \n";
+                        echo "Deleted Product Photo >>>> " . public_path('storage/product/' . $photo['thumb']) . " \n";
+                    } else {
+                        echo "Skipped Product Photo >>>> {$photo['image']} \n";
 
-                    if (empty($product->photo) || !file_exists(public_path('storage/product/' . $product->photo))) {
-                        echo "Updating Product Photo >>>> {$product->name} \n";
-                        $product->photo = $photos[0]['image'];
+                        if (empty($product->photo) || !file_exists(public_path('storage/product/' . $product->photo))) {
+                            echo "Updating Product Photo >>>> {$product->name} \n";
+                            $product->photo = $photo['image'];
+                        }
+                        if (empty($product->thumbnail) || !file_exists(public_path('storage/product/' . $product->thumbnail))) {
+                            echo "Updating Product Thumb >>>> {$product->name} \n";
+                            $product->thumbnail = $photo['thumb'];
+                        }
+
+                        $product->save();
                     }
-                    if (empty($product->thumbnail) || !file_exists(public_path('storage/product/' . $product->thumbnail))) {
-                        echo "Updating Product Thumb >>>> {$product->name} \n";
-                        $product->thumbnail = $photos[0]['thumb'];
-                    }
-
-                    $product->save();
                 }
             } else {
                 if (empty($product->photo) || !file_exists(public_path('storage/product/' . $product->photo)) ||
