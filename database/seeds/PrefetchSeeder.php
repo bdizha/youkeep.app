0ed90adfd6793d81304953ccda2010214bb88d3a
Batanayi Matuku <bata@graphigem.com>
2021-05-06 18:15:35 -0700

    Added welcome assets

--- database/seeds/PrefetchSeeder.php
+++ database/seeds/PrefetchSeeder.php
@@ -15,10 +15,16 @@ class PrefetchSeeder extends Seeder
             ->orderBy('updated_at', 'ASC')
             ->get();
 
+        $url = config('app.api_url', config('app.url')) . "/api/categories";
+
+        file_get_contents($url);
+
         foreach ($this->storeCategories as $storeCategory) {
             $level = $storeCategory->level;
+            $url = url("/api" . $storeCategory->category->route . "/L" . str_pad($level, 6, STR_PAD_LEFT, "0"));
 
-            $url = config('app.client_url', config('app.url')) .  $storeCategory->category->route . "/L" . str_pad($level, 6, STR_PAD_LEFT, "0");
+            echo "Fetching category: " . $storeCategory->category->route . "\n";
+
             $content = file_get_contents($url);
 
 //            dd($content);
