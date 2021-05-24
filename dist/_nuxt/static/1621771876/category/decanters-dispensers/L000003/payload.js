bc0058ea5087274053bde28048995bcd9273d08e
Batanayi Matuku <bata@graphigem.com>
2021-05-23 17:20:28 -0700

    Added global categories

---
+++ dist/_nuxt/static/1621771876/category/decanters-dispensers/L000003/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/category/decanters-dispensers/L000003", (function(a,b){return {data:[{}],fetch:{},mutations:[[a,{key:"isCategory",value:b}],[a,{key:"isProduct",value:b}],[a,{key:"isFixed",value:b}],["base\u002FsetCategories",[]],["base\u002FsetStores",[]],["base\u002FsetProducts",{data:[]}],[a,{key:"isTray",value:b}]]}}("base\u002FsetProcess",true)));
---
+++ dist/_nuxt/static/1621771876/category/plain/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/category/plain", (function(a,b,c){return {data:[{}],fetch:{},mutations:[[a,{key:"isCategory",value:b}],[a,{key:"isProduct",value:b}],[a,{key:c,value:b}],["base\u002FsetCategories",[]],["base\u002FsetStores",[]],["base\u002FsetProducts",{data:[]}],[a,{key:c,value:false}],[a,{key:"isTray",value:b}]]}}("base\u002FsetProcess",true,"isFixed")));
