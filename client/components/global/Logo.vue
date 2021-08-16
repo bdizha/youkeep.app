fb1a3d340f5c430d16f0a2bb6ca594ff1b941bfa
Batanayi Matuku <bata@graphigem.com>
2021-08-17 01:55:20 +0200

    Optimizing images

--- client/components/global/Logo.vue
+++ client/components/global/Logo.vue
@@ -1,9 +1,9 @@
 <template>
   <nuxt-link :class="{'r-logo__icon': isIcon}" class="r-logo" to="/">
-    <img v-if="isIcon && isDark" :alt="title" src="~/assets/icon_white.svg"/>
-    <img v-if="isIcon && !isDark" :alt="title" src="~/assets/icon.svg"/>
-    <img v-if="!isIcon && isDark" :alt="title" src="~/assets/logo_white.svg"/>
-    <img v-if="!isIcon && !isDark" :alt="title" src="~/assets/logo.svg"/>
+    <img v-if="isIcon && isDark" :alt="title" src="/images/icon_white.svg"/>
+    <img v-if="isIcon && !isDark" :alt="title" src="/images/icon.svg"/>
+    <img v-if="!isIcon && isDark" :alt="title" src="/images/logo_white.svg"/>
+    <img v-if="!isIcon && !isDark" :alt="title" src="/images/logo.svg"/>
   </nuxt-link>
 </template>
 <script>
